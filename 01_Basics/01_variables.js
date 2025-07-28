a86b8808ef92e88eb45e47e4a4706e1f74b508b3
LalanSamrit <165518566+LalanSamrit@users.noreply.github.com>
2025-07-28 18:49:19 +0000

    datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+let accountEmail = "lalansamrit@gmail.com"
+var accountPassword = "0123"
+accountCity = "Nagpur"
+let accountState;
+
+// accountId = 23;
+console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in block scope and funtional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strick";  // treat all JS code as never version
+
+// alert( 3 + 3)  //we are using Nodejs, not browser
+
+let name = "lalan"
+let age = 21
+let isLoginedIn = false
+
+/*
+primitive data type
+number  => 2 to power 53
+bigint  => greater than int
+string  => "anything"
+boolean => T/F
+null  => standalone value 
+undefined  => value is not assign
+symbbol  => find uniqueness 
+*/
+
+// objects
+
+console.log(typeof 3);
+console.log(typeof "lalan" );
+console.log(typeof undefined);
+console.log(typeof null);
+
