a2882e0bbed7a888eefd5f6c3654f1d714d60c9b
LalanSamrit <165518566+LalanSamrit@users.noreply.github.com>
2025-07-29 17:15:28 +0000

    Conversion od datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+// let score = "33abc";
+// let s1 = null;
+// let s2 = undefined;
+// let s3 = 33;
+// let s4 = true;
+
+// console.log(typeof score);     //string
+// console.log(typeof(score));    //string
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);   //number
+// console.log(valueInNumber);    //NaN
+// console.log(typeof s1);       //object
+// console.log(s1);             // null
+// console.log(typeof s2);      //undefined
+// console.log(s2);             //undefined
+// console.log(typeof s3);     //number
+// console.log(s3);           //33
+// console.log(typeof s4);   //boolean
+// console.log(s4);          //true
+
+
+let isLoggedIn = "lalan";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+let someNumber = 21;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
