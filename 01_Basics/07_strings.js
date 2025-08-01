760538f85c8c57f6ce8ff73387eb93278116c753
LalanSamrit <165518566+LalanSamrit@users.noreply.github.com>
2025-08-01 19:03:53 +0000

    strings

---
+++ 01_Basics/07_strings.js
@@ -0,0 +1,27 @@
+const name = "  lalan  ";
+const repoCount = 5;
+
+// console.log(name + repoCount + " value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('lalan');
+
+// console.log(gameName);
+// console.log(typeof gameName);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.lastIndexOf());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('n'));
+// console.log(gameName.toString());
+
+// console.log(gameName.trim());
+// console.log(gameName.substring(0,4));
+// console.log(gameName.slice(0,3));
+
+// console.log(gameName.replaceAll('a', 'x'));
+// console.log(gameName.includes('lalan'));
+// console.log(gameName.split('s'));
