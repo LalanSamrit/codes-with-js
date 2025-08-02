7dae6c171bd6efd9f1cdffb28757100e79f04670
LalanSamrit <165518566+LalanSamrit@users.noreply.github.com>
2025-08-02 09:42:29 +0000

    num and math

---
+++ 01_Basics/08_nums_and_math.js
@@ -0,0 +1,38 @@
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 23.8966;
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 100000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// +++++++++++++ MATHS +++++++++++++++//
+
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));   // Round off
+console.log(Math.ceil(4.6));    //High Round off
+console.log(Math.floor(4.9));   //Low Round off
+
+console.log(Math.min(4, 6, 3, 7, 1, 9));
+console.log(Math.max(4, 6, 3, 7, 1, 9));
+
+console.log(Math.random());  //Random value bet 0 to 1
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
