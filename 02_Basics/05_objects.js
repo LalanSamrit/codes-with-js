4515ec4ea3512b94ee9354dcd253fd75b21752f5
LalanSamrit <165518566+LalanSamrit@users.noreply.github.com>
2025-08-03 10:24:28 +0000

    objects part 3

---
+++ 02_Basics/05_objects.js
@@ -0,0 +1,29 @@
+const course = {
+    coursename: "java script",
+    price: "999",
+    courseInstructor: "lalan"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+console.log(instructor);
+
+const navbar = (company) => {
+
+}
+
+navbar(company = "lalan")
+
+///JSON file structure
+// {
+//     "name": "lalan",
+//     "coursename": "java script",
+//     "price": "free"
+// }
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
