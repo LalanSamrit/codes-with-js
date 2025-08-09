4904ddefa4ad79d37fbfd177febfc3854325869c
LalanSamrit <165518566+LalanSamrit@users.noreply.github.com>
2025-08-09 08:27:51 +0000

    scopes

--- 03_Basics/03_scopes.js
+++ 03_Basics/03_scopes.js
@@ -1,17 +1,62 @@
-let a = 100
-const b = 200
-var c = 300
+// let a = 100
+// const b = 200
+// var c = 300
 
-if(true) {
-    let a = 10
-    const b = 20
-    var c = 30
-}
+// if(true) {
+//     let a = 10
+//     const b = 20
+//     var c = 30
+// }
 
-console.log(a); 
-console.log(b);
-console.log(c);
+// console.log(a); 
+// console.log(b);
+// console.log(c);
 
 // for(let i = 0; i < Array.length; i++) {
 //     const element = array[i];
 // }
+
+
+
+// function one() {
+//     const username = "lalan"
+
+//     function two() {
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     // console.log(website);
+    
+//     two()
+// }
+
+// one()
+
+
+
+// if (true) {
+//     const username = "lalan"
+//     if (username === "lalan") {
+//         const website = " youtube"
+//         console.log(username + website);
+//     }
+//     // console.log(website); 
+// }
+// // console.log(username);
+
+
+
+////////////// Intersting /////////////////////////////
+
+function addone(num) {
+    return num + 1
+}
+addone(5)
+console.log(addone(5));
+
+
+const addTwo = function (num) {
+    return num + 2
+}
+addTwo(5)
+console.log(addTwo);
