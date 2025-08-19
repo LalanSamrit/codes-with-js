32abd786778396d91175551da59ad41575d7473d
LalanSamrit <165518566+LalanSamrit@users.noreply.github.com>
2025-08-19 03:53:20 +0000

    while and do-while loop

---
+++ 05_Iterations/dowhileloop.js
@@ -0,0 +1,12 @@
+let i = 0;
+while (i <= 10) {
+    console.log(`value ${i}`);
+    i = i + 2;
+}
+
+
+let score = 1;
+do {
+    console.log(`print score ${score}`)
+    score++
+} while (score <= 10);
