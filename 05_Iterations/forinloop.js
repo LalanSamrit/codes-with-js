82a6f92a32ac30acc935af7113a754f90181aef6
LalanSamrit <165518566+LalanSamrit@users.noreply.github.com>
2025-08-19 14:41:51 +0000

    for in loop

---
+++ 05_Iterations/forinloop.js
@@ -0,0 +1,16 @@
+// const myObj = {
+//     js: "javascript",
+//     cpp: "c++",
+//     rb: "ruby"
+// }
+// for (const key in myObj) {
+// //    console.log(myObj[key]);
+//    console.log(`${key} shortcut is for ${myObj[key]}`);
+// }
+
+const programming = ["js", "java", "cpp", "rb", "py"]
+for (const key in programming) {
+    // console.log(key); 
+    console.log(programming[key]);
+}
+
